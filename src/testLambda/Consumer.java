77f2e7d41e589ed1c6b757048ce13a7889ffa7a3
CatIsland123 <1289685016@qq.com>
2019-04-30 19:40:21 +0800

    第一题

--- src/testLambda/Consumer.java
+++ src/testLambda/Consumer.java
@@ -1,5 +1,14 @@
 package testLambda;
 
-public interface Consumer {
+import java.util.Objects;
 
+@FunctionalInterface
+public interface Consumer<T> {
+
+    void accept(T t);
+
+    default Consumer<T> andThen(Consumer<? super T> after) {
+        Objects.requireNonNull(after);
+        return (T t) -> { accept(t); after.accept(t); };
+    }
 }
--- src/testLambda/appleTest.java
+++ src/testLambda/appleTest.java
@@ -1,10 +1,39 @@
 package testLambda;
+class Apple{
+	String color;
+	double weight;
+	Apple(String c,double w){
+		color=c;
+		weight=w;
+	}
+}
 
 public class appleTest {
+	public static void ConsumerApple (Apple[] apps,Consumer<Apple> c){
+		 for(Apple app:apps){
+			 c.accept(app);
+		 }
+	 }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 Apple [] aps = null;
+		 Apple a=new Apple("red",12.3);
+		 Apple a1=new Apple("yellow",11.1);
+		 aps[0]=a;
+		 aps[1]=a1;
+	     appleTest lmd=new appleTest();
+	     lmd.ConsumerApple(aps, (apps)->{
+	    	 for(int i=0;i<aps.length;i++)
+	    		 return;
+	    	 
+	     });
+	     
 
 	}
 
+	private void ConsumerApple(Apple[] aps, Consumer<Apple> c) {
+		// TODO Auto-generated method stub
+		
+	}
 }
